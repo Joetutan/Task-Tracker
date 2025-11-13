43e34c2739032437f5c0dab1577b9726840b80b0
joetutan <noobianjoe@outlook.com>
2025-11-13 21:14:46 +0200

    Final touch and testing

--- core/core_functs.py
+++ core/core_functs.py
@@ -1,61 +1,176 @@
 from pathlib import Path
+from datetime import datetime
 import json
 
-file = Path("./data/data.json")
 
-if file.exists():
-                with open(file, "r") as f:
-                        tasks = json.load(f)
+
+# check if file exists in dir. Read file and assign to tasks if true else create new dict 
+file = Path("./data/data.json") # dir to save and retreive json file
+
+if file.exists(): 
+        with open(file, "r") as f:
+                    tasks = json.load(f)
 else:
         tasks = {}
+    
 
-def add_task(des: str, status: str, timestamp: str) -> str:
-        ID = len(tasks) + 1
+# keep record of current timestamp
+timestamp = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+
+def add_task(des: str) -> str:
+        '''
+        -Assign unique task ID.
+        -Increment ID in while loop to avoid assigning duplicate key after deletion.
+        -Assign status 'to-do' as default for new tasks.
+        -Write to json file after updating task properties.
+        -Print success message to cli
+        '''
+        ID = len(tasks) + 1 
         while str(ID) in tasks:
                 ID += 1
+
+        status = 'to-do' 
         tasks[ID] =  { 
-                    'ID: ' : ID,
-                    'des' : des, 
-                     'status': status, 
-                     'createdAt' : timestamp,
-                     'updatedAt' : None
+                        'ID: ' : ID,
+                        'des' : des, 
+                        'status': status, 
+                        'createdAt' : timestamp,
+                        'updatedAt' : None
                         }
+        
         with open(file, "w") as f:
                 json.dump(tasks, f, indent=4)
                         
                 
         return print(f"Task added successfully! (ID: {ID})")
 
-def list_tasks():
-        if tasks:
-                for i in tasks:
-                        print(f"{i} : {tasks[i]['des']}")
-        else:
+def list_tasks(task_property):
+        '''
+        -List tasks with matching task property in dict sorted by Unique IDs
+        '''
+        
+        s = {'all', 'to-do', 'in-progress', 'done'}
+        if task_property not in s:
+                print("Invalid input: please try again")
+                return "fail"
+        if not tasks:
                 print('Please add tasks..')
-
-def del_task(dl: str):
+                return "pass"
         
-        del tasks[dl]
+        match task_property:
+                case 'all':
+                        
+                        for i in sorted(tasks, key=lambda x: int(x)):
+                                print(f"{i} : {tasks[i]['des']}")
+                        return "pass"
+                
+                case 'to-do':
+                        count = sum(t['status'] == task_property for t in tasks.values())
+                        if count > 0:
+                                list_helper(task_property)
+                        else:
+                                print("None...")
+                                return "pass"
+                
+                case 'in-progress':
+                        count = sum(t['status'] == task_property for t in tasks.values())
+                        if count > 0:
+                                list_helper(task_property)
+                        else:
+                                print("None...")
+                                return "pass"
 
-        with open(file, "w") as f:
-                        json.dump(tasks, f, indent=4)
-        return print(f"Task deleted successfully! (ID: {dl})")
+                case 'done':
+                        count = sum(t['status'] == task_property for t in tasks.values())
+                        if count > 0:
+                                list_helper(task_property)
+                        else:
+                                print("None...")
+                                return "pass"
 
-def update_task(task_id, des, timestamp):
-        tasks[task_id]["des"] = des
-        tasks[task_id]["updatedAt"] = timestamp
-        with open(file, "w") as f:
-                        json.dump(tasks, f, indent=4)
-        return print(f"Task updated successfully! (ID: {task_id})")
+def list_helper(task_property):
+        '''
+                -Helper function for list_tasks() to avoid repetition
+        '''
+        for i in sorted(tasks, key=lambda x: int(x)):
+                        if tasks[i]['status'] == task_property:
+                                print(f"{i} : {tasks[i]['des']}")
+        return "pass"
+                
+                        
 
-def mark_task(task_id, status, timestamp):
-        tasks[task_id]["status"] = status
-        tasks[task_id]["updatedAt"] = timestamp
-        with open(file, "w") as f:
+def del_task(dl):
+        '''
+        -delete task with given ID key
+        -update the change in json file
+        -print success message  to cli
+        '''
+        while True:
+                if dl not in tasks:
+                        print(f"Task {dl} not found. Please try again")
+                        return "fail"
+                del tasks[dl]
+                with open(file, "w") as f:
                         json.dump(tasks, f, indent=4)
-        return print(f"Task marked as {status} successfully! (ID: {task_id})")
-        
-        
+                print(f"Task deleted successfully! (ID: {dl})")
+                return "pass"
+
+def update_task(task_id):
+        '''
+        -assign description property change for task with provided ID as key
+        -update change updated timestamp
+        -write change to json file
+        -print success message
+        '''
+        while True:
+                if task_id not in tasks:
+                        print(f"Task {task_id} not found. Please try again")
+                        return "fail"
+                
+                des = input("Enter update:... ")
+                tasks[task_id]["des"] = des
+                tasks[task_id]["updatedAt"] = timestamp
+
+                with open(file, "w") as f:
+                                json.dump(tasks, f, indent=4)
+                print(f"Task updated successfully! (ID: {task_id})")
+                return "pass"
+
+
+def mark_task(task_id):
+        '''
+        -Validate input if true continue else print fail message
+        -assign status property change for task with provided ID as key
+        -update change updated time stamp
+        -write change to json file
+        -print success message
+        '''
+        while True:
+                if task_id not in tasks:
+                        print(f"Task {task_id} not found. Please try again")
+                        return "fail"
+
+                status = input("mark-task: (to-do/in-progress/done) ").strip().lower()
+                s = {"to-do","in-progress", "done"}
+                if status not in s :
+                        print("Invalid input: please enter: (to-do/in-progress/done)")
+                        continue
+                
+                tasks[task_id]["status"] = status
+                tasks[task_id]["updatedAt"] = timestamp
+
+                with open(file, "w") as f:
+                        json.dump(tasks, f, indent=4)
+
+                print(f"Task marked as {status} successfully! (ID: {task_id})")
+                return "pass"
+                
+                
+                        
+                                
+                
+                        
+
 
 
 
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
 
-from datetime import datetime
+
 from core.core_functs import add_task, list_tasks, del_task, update_task, mark_task
 
+
 if __name__ == "__main__":
+
     
     while True:
-        cmd = input('Chose action (Add/Update/Mark/Delete/List/Exit): ').strip().lower()
-        timestamp = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+        cmd = input('Choose action (Add/Update/Mark/Delete/List/Exit): ').strip().lower()
+        
         match cmd:
             case 'add':
                 des = input('Enter task description:... ').strip()
-                status = 'to-do'
-                add_task( des, status, timestamp)
+                add_task(des)
 
             case 'list':
-                list_tasks()
-
+                while True:
+                    
+                    task_property = input("Choose (all/to-do/in-progress/done): ").strip().lower()
+                    result = list_tasks(task_property)
+                    if result == "fail":
+                        continue
+                    else:
+                        break
             case 'delete':
-                task_id = input("Please enter task ID: ")
-                del_task(task_id)
-
+                while True:
+                    try:
+                        task_id = int(input("Please enter task ID: ").strip())
+                        result = del_task(str(task_id))
+                        if result == "fail":
+                            continue
+                        if result == "pass":
+                            break
+                    except ValueError:
+                        print("Invalid input: please enter a number")
             case 'exit':
                 print("Goodbye")
                 break
 
             case 'update':
-                task_id = input("Please enter task ID: ")
-                change = input("Enter update:... ")
-                update_task(task_id, change, timestamp)
+                while True:
+                    try:
+                        task_id = int(input("Please enter task ID: ").strip())
+                        update_task(str(task_id))
+                        break
+                    except ValueError:
+                        print("Invalid input: please enter a number")
+
             case 'mark':
-                s = {"to-do","in-progress", "done"}
-                task_id = input("Please enter task ID: ")
-                status = input("mark-task: \n to-do \n in-progress \n done \n")
-                if status in s:
-                    mark_task(task_id , status, timestamp)
-                else:
-                    print('wrong entry please try again')
+                while True:
+                    try:
+                        task_id = int(input("Please enter task ID: "))
+                        result = mark_task(str(task_id))
+                        if result == "fail":
+                            continue
+                        if result == "pass":
+                            break
+                    except ValueError:
+                        print("Invalid input: please enter a number")
+
+
             case _:
                 print('Unknown input')
